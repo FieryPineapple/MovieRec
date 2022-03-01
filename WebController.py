a24b5e11a3c73719b6c33d920d38fb320679ca75
aslee28 <81182187+aslee28@users.noreply.github.com>
2022-03-01 14:37:40 -0800

    added my #2 - Alex

--- WebController.py
+++ WebController.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -15,5 +16,13 @@ title = soup.title.text
 @app.route('/qw')
 def qw():
     return title
+
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"password123")
+
+@app.route('/alex')
+def alex():
+  return token
 
 app.run(host = "0.0.0.0")
