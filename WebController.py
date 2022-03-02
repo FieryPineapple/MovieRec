aa49472fd005174f2e0b89584036018beca6dc2c
rosalindach28 <rosalindaa.chamale@gmail.com>
2022-03-01 16:51:47 -0800

    Added new method using library

--- WebController.py
+++ WebController.py
@@ -2,6 +2,7 @@ from flask import Flask
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from cryptography.fernet import Fernet
+import math
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -24,5 +25,13 @@ token = f.encrypt(b"password123")
 @app.route('/alex')
 def alex():
   return token
+
+@app.route('/rc', methods = ["GET"])
+def rc():
+    user_num1 = int(input("Enter first number: "))
+    user_num2 = int(input("Enter second number: "))
+    result = math.gcd(user_num1, user_num2)
+    return ("The greatest common divisor of " + str(user_num1) + " and " + str(user_num2) + " is " + str(result))
+
 
 app.run(host = "0.0.0.0")
