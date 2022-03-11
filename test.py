9e778f834a72e68baba9cc23eca424cf7e5bd25a
qinyiwang11 <56134685+qinyiwang11@users.noreply.github.com>
2022-03-10 20:02:00 -0800

    test actor and movie search

--- test.py
+++ test.py
@@ -1,7 +1,30 @@
 import unittest
 from imdb import Cinemagoer
 
+cg = Cinemagoer()
+person = cg.search_person('keanu reeves')
+movie = cg.search_movie('matrix')
+
 class Test(unittest.TestCase):
-    def test_search(self):
-        cg = Cinemagoer()
-        person = cg.search_person('keanu reeves')
+    def test_actor_search(self):
+        self.assertEqual(person[0]['name'], 'Keanu Reeves')
+        self.assertEqual(person[3]['name'], 'Keanu Reeves')
+    
+    def test_movie_search(self):
+        self.assertEqual(movie[0]['title'], 'The Matrix')
+        self.assertEqual(movie[4]['title'], 'The Matrix')
+        self.assertRegexpMatches(movie[4]['title'], 'Matrix')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+"""
+#Check list
+for p in person:
+    print(p['name'])
+
+for m in movie:
+    print(m['title'])
+""" 
