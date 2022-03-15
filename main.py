608591dd9705c462144dba6f35699076828aebae
qinyiwang11 <56134685+qinyiwang11@users.noreply.github.com>
2022-03-14 22:36:43 -0700

    work in progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from re import template
-from flask import Flask, render_template, request
+import re
+from flask import Flask, render_template, request, redirect, url_for, session
 import cg
 from imdb import Cinemagoer
 #from justwatch import JustWatch
@@ -7,19 +8,53 @@ from imdb import Cinemagoer
 app = Flask(__name__, template_folder = 'template')
 app.config["DEBUG"] = True
 
-ia = cg.search()
+#ia = cg.search()
+cg = Cinemagoer()
 #jw = JustWatch(country='US')
+
+search_name = "keanu reeves"
+search_type = "Actor/Actress"
+result_list = ""
+name_list = []
 
 @app.route('/')
 def home():
     return render_template('home.html')
-    #return "Welcome"
 
 @app.route('/search', methods = ['GET', 'POST'])
 def search():
-    name = request.args.get('text')
     types = ['Actor/Actress', 'Movie', 'Director', 'Production Company']
+    # if request.method == 'POST':
+    #     search_type = request.form.get['types']
+    #     search_name = request.form.get['search']
+    #     session["types"] = search_type
+    #     session["search"] = search_name
+    #     return redirect(url_for('results', search_type=search_type, search_name=search_name))
     return render_template('search.html', types=types)
-    #return actor_name
+        #return actor_name
+
+@app.route('/results', methods = ['POST'])
+def results():
+    # search_type = session["types"]
+    # search_name = session["search"]
+    if request.method == 'POST':
+        if search_type == 'Actor/Actress':
+            result_list = cg.search_person(search_name)
+            for person in result_list:
+                name = person['name']
+                name_list.append(name)
+
+        if search_type == 'Movie':
+            result_list = cg.search_movie(search_name)
+            for movie in result_list:
+                name = movie['title']
+                name_list.append(name)
+    
+    return render_template('results.html', search_name=search_name, name_list=name_list)
+
+
+@app.route('/mystuff')
+def mystuff():
+    return render_template('mystuff.html')
 
 app.run(host = "0.0.0.0")
