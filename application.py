be1ae8799df5532c7d9e500779fc3b083b114f5b
qinyiwang11 <56134685+qinyiwang11@users.noreply.github.com>
2022-04-10 18:07:07 -0700

    working on information

--- application.py
+++ application.py
@@ -14,11 +14,14 @@ cg = Cinemagoer()
 search_name = ""
 search_type = ""
 result_list = ""
+name = ""
 name_list = []
 
 @application.route('/')
 def home():
-    return render_template('home.html')
+    chart = cg.get_popular100_movies()
+    chart = chart[0:20]
+    return render_template('home.html', chart=chart)
 
 @application.route('/search', methods = ['GET', 'POST'])
 def search():
@@ -29,6 +32,7 @@ def results():
     search_type = request.form.get("choice")
     search_name = request.form.get("search")
     if request.method == 'POST':
+        name_list.clear()
         if search_type == 'actor':
             result_list = cg.search_person(search_name)
             for person in result_list:
@@ -40,9 +44,28 @@ def results():
             for movie in result_list:
                 name = movie['title']
                 name_list.append(name)
+        
+        #Separate actors from directors???
+        if search_type == 'director':
+            result_list = cg.search_person(search_name)
+            for person in result_list:
+                name = person['name']
+                name_list.append(name)
+
+        if search_type == 'company':
+            result_list = cg.search_company(search_name)
+            for person in result_list:
+                name = person['name']
+                name_list.append(name)
     
     return render_template('results.html', search_name=search_name, name_list=name_list)
 
+@application.route('/<name>', methods = ['POST', 'GET'])
+def info(name):
+    #name = request.form.get("cname")
+    name="some name"
+    #getInfo = cg.get_person(name.getID(), info=['main'])
+    return render_template('information.html', name=name)
 
 @application.route('/mystuff')
 def mystuff():
