b61d88638ac8b8f996fa5977f355ff99ed95e7d4
rosalindach28 <rosalindaa.chamale@gmail.com>
2022-05-02 13:21:41 -0700

    Can now print list of providers based on country

--- jw.py
+++ jw.py
@@ -1,9 +1,10 @@
 from justwatch import JustWatch
 
-just_watch = JustWatch(title_id=10)
+just_watch_obj = JustWatch(country='US')
+provider_count = 0 # keep track of amount of providers available in country
 
-results = just_watch.get_providers()
-for r in results:
-    #x = slice(3, 4)
-    prov = r[:2, :2]
-print(prov)
+#prints list of service providers based on country 
+for result in just_watch_obj.get_providers():
+    print(result.get('clear_name')) # prints full name of service provider
+    provider_count = provider_count + 1
+#print(provider_count)
